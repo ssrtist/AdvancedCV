35e87125abe11630428bc06373c5639e80382cfe
ssrtist <ssrtist@yahoo.com>
2022-12-10 21:57:13 -0500

    lesson1 #1

--- HandTracking/HandTrackingMin.py
+++ HandTracking/HandTrackingMin.py
@@ -4,9 +4,19 @@ import time
 
 cap = cv2.VideoCapture(0)
 
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
 while True:
     success, img = cap.read()
+    imRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imRGB)
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            mp.solutions.drawing_utils.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+            #print(handLms)
 
     cv2.imshow('Image', img)
-    cv2.waitKey(1)
+    if cv2.waitKey(1) == ord('q'):break
 
