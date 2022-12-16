39258adc73df2cf75a9592ffe3de37a9b3991e37
ssrtist <ssrtist@yahoo.com>
2022-12-15 23:40:45 -0500

    chapter1 #4

--- HandTracking/HandTrackingMin.py
+++ HandTracking/HandTrackingMin.py
@@ -16,6 +16,8 @@ cTime = 0
 
 while True:
     success, img = cap.read()
+    cv2.flip(img, 1, img)
+
     imRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imRGB)
     if results.multi_hand_landmarks:
@@ -24,12 +26,12 @@ while True:
                 # print(id,lm)
                 h,w,c=img.shape
                 cx,cy=int(lm.x*w),int(lm.y*h)
-                print(id,': ',cx,cy)
                 if id==0:
-                    cv2.circle(img, (cx, cy), 25, (255,0,255))
+                    print(id,': ',cx,cy)
+                    cv2.circle(img, (cx, cy), 25, (255,0,255), cv2.FILLED)
+
             mp.solutions.drawing_utils.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
             #print(handLms)
-    cv2.flip(img, 1, img)
 
     cTime = time.time()
     fps = 1/(cTime - pTime)
